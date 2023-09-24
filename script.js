434e8e4272c9d9cd356084e34dfd1e2a85622ace
EMLdeveloper <EnmanuelleML@outlook.com>
2023-09-24 02:10:50 -0700

    primero

--- script.js
+++ script.js
@@ -24,11 +24,9 @@ let navbar = document.querySelector(".navbar");
 
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
-  loginForm.classList.remove("active");
 };
 
 window.onscroll = () => {
-  loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
 
